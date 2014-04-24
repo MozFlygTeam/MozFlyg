af645e1a7859f1903324ff10646932bd67ac5c5f
kimerik <kim.erik88@gmail.com>
2014-04-24 12:41:54 +0200

    MainClass
    Skapat mainfunktionen

---
+++ MozFlyg.java
@@ -0,0 +1,12 @@
+package MozFlyg;
+
+import javax.swing.JOptionPane;
+
+public class MozFlyg {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		JOptionPane.showMessageDialog(null, "Test");
+	}
+
+}
