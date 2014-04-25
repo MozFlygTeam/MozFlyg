0ea48525a1b16d82ba87bd357455600407a29bb8
Timalex <tim.alexander.gunnarsson@gmail.com>
2014-04-25 10:50:57 +0200

    Nytt git projekt
    Passar också inuti ett eclipse-projekt

---
+++ src/models/AirportModel.java
@@ -0,0 +1,27 @@
+package models;
+
+public class AirportModel
+{
+	private int id;
+	private String airportName;
+	private String cityName;
+	
+	public int getId() {
+		return id;
+	}
+	public String getAirportName() {
+		return airportName;
+	}
+	public String getCityName() {
+		return cityName;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public void setAirportName(String airportName) {
+		this.airportName = airportName;
+	}
+	public void setCityName(String cityName) {
+		this.cityName = cityName;
+	}
+}
