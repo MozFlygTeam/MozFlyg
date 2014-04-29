22384e080348153821169ef06798a56ff1ce3d8d
Magnus Karlsson <magnus_karlsson@live.com>
2014-04-29 14:24:08 +0200

    Made some logic for login

--- src/GUI/Login.java
+++ src/GUI/Login.java
@@ -8,6 +8,7 @@ import java.awt.event.ActionListener;
 import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.SwingUtilities;
@@ -18,28 +19,40 @@ public class Login extends JDialog implements ActionListener{
 	private static final String LOGIN = "login";
 	private static final String CANCEL = "cancel";
 
+	private String user = "123";
+	private String pass = "321";
+	
+	JTextField userFld;
+	JTextField passFld;
+	
+	
 	public Login(){
         
         JPanel dataPnl = new JPanel(new GridLayout(3, 2, 5, 5));
         dataPnl.setBorder(new EmptyBorder(5, 5, 5, 5));
         add(dataPnl, BorderLayout.CENTER);
         
+        //Create username field
         dataPnl.add(new JLabel("Användarnamn"));
-        JTextField userFld = new JTextField(10);
+        userFld = new JTextField(10);
         dataPnl.add(userFld);
         
+        //Create password field
         dataPnl.add(new JLabel("Lösenord"));
-        JTextField passFld = new JTextField(10);
+        passFld = new JTextField(10);
         dataPnl.add(passFld);
         
+        //Create panel
         JPanel bottomPnl = new JPanel();
         add(bottomPnl, BorderLayout.PAGE_END);
         
+        //Create login button
         JButton btn = new JButton("Logga in");
         btn.setActionCommand(LOGIN);
         btn.addActionListener(this);
         bottomPnl.add(btn);
         
+        //Create cancel button
         btn = new JButton("Cancel");
         btn.setActionCommand(CANCEL);
         btn.addActionListener(this);
@@ -48,11 +61,55 @@ public class Login extends JDialog implements ActionListener{
         pack();
 	}
 	
+	public String getUser(){
+		return user;
+	}
+	
+	public String getPass(){
+		return pass;
+	}
+	
+	public boolean loginVerification(){
+		
+		
+		
+		String inputUser = userFld.getText();
+		String inputPass = passFld.getText();
+		boolean loggedIn = false;
+		
+		if(inputUser.equals(getUser()) && inputPass.equals(getPass())){
+			loggedIn = true;
+		}
+		return loggedIn;
+	}
+	
+	public void checkIfLoggedIn(){
+		if(loginVerification() == true){
+			System.out.println("You are logged in!!! :D");
+		}
+	}
+	
 	public void actionPerformed(ActionEvent event){
 		String action = event.getActionCommand();
 		
-		if (action.equals(CANCEL)) {
+		if (action.equals(CANCEL)){
 			System.exit(0);
-        }
+		}
+		
+		else if(action.equals(LOGIN)){
+			if(loginVerification()){
+				checkIfLoggedIn();
+			}
+		}
 	}
+	
+	public static void main(String[] args) {
+		SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+				Login frame = new Login();
+				frame.setVisible(true);
+			}
+		});
+	}
+
 }
