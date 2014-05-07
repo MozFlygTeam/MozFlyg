66a1fa3e2bd23ed74a1c937939d505d19e046e38
kimerik <kim.erik88@gmail.com>
2014-05-07 10:55:22 +0200

    Lagt till add och remove

--- src/GUI/Airport.java
+++ src/GUI/Airport.java
@@ -23,10 +23,9 @@ import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.ListSelectionListener;
 import javax.swing.table.AbstractTableModel;
 
+public class Airport extends JFrame implements ActionListener,
+		ListSelectionListener {
 
-public class Airport extends JFrame implements ActionListener, ListSelectionListener
-{
-	
 	public DBModelAirport airportModel;
 	public static TableModelAirport table;
 	private JButton removeButton;
@@ -34,78 +33,69 @@ public class Airport extends JFrame implements ActionListener, ListSelectionList
 	private static final String ADD = "add";
 	private static final String DELETE = "delete";
 	
-	
-	public Airport()
-	{
-		
-    	
+	public Airport() {
+
 		table = new TableModelAirport();
 		airportModel = new DBModelAirport();
 		recordTable = new JTable(table);
 		table.getAll();
-		
+
 		JPanel contentPane = new JPanel(new BorderLayout());
 		JScrollPane scrollPane = new JScrollPane(recordTable);
 		JPanel bottomPanel = new JPanel(new FlowLayout());
-			
+
 		JButton addButton = new JButton("Lägg till");
-			addButton.addActionListener(this);
-			addButton.setActionCommand(ADD);
-			
+		addButton.addActionListener(this);
+		addButton.setActionCommand(ADD);
+
 		removeButton = new JButton("Ta Bort");
 		removeButton.addActionListener(this);
 		removeButton.setActionCommand(DELETE);
-			
 
 		setContentPane(contentPane);
 		add(scrollPane, BorderLayout.CENTER);
 		add(bottomPanel, BorderLayout.PAGE_END);
 		bottomPanel.add(addButton);
 		bottomPanel.add(removeButton);
-	    
+
 		pack();
 		setLocationRelativeTo(null);
 	}
-	
+
 	// funktionen f�r att l�gga till airport
-    private void addAirport() {
-    	
-    	AddAirport add = new AddAirport();
-    	if(add.getAirport() != null){}
-    		add.setVisible(true);
-    		table.addAirport(add.getAirport());
+	private void addAirport() {
+
+		AddAirport add = new AddAirport();
+		add.setVisible(true);
+		
+		DBModelAirport model = add.getAirport();
+
+		if (model.insert() == 1) {
+			table.addAirport(model);
+		}
 	}
-    
-    // Ska skapas
-    private void deleteAirport()
-    {
-    	
-    }
-    
+
+	// Ska skapas
 
 	@Override
-	public void actionPerformed(ActionEvent event)
-	{
-	
-		switch (event.getActionCommand())
-		{
-			case ADD: 
-				addAirport();
-				break;
-			case DELETE: 
-				deleteAirport();
-				break;
-	
+	public void actionPerformed(ActionEvent event) {
+
+		switch (event.getActionCommand()) {
+		case ADD:
+			addAirport();
+			break;
+		case DELETE:
+			int i = recordTable.getSelectedRow();
+			table.removeAirport(i);
+			break;
+
 		}
 	}
 
 	@Override
-	public void valueChanged(ListSelectionEvent arg0)
-	{
+	public void valueChanged(ListSelectionEvent arg0) {
 		// TODO Auto-generated method stub
 		removeButton.setEnabled(recordTable.getSelectedRow() >= 0);
 	}
-	
-		
-}
 
+}
