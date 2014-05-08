bd5f6fa2331aedd0010143f82d206163c52f2e76
Timalex <tim.alexander.gunnarsson@gmail.com>
2014-05-08 12:40:08 +0200

    Skapa flight-tabell

--- sql-skript/SQL-mozflyg.sql
+++ sql-skript/SQL-mozflyg.sql
@@ -1,25 +1,37 @@
 DROP TABLE account, airport;
 
-CREATE TABLE airport (
+CREATE TABLE airport
+(
 id SMALLINT(5) AUTO_INCREMENT NOT NULL,
-name CHAR(128) DEFAULT NULL,
-city CHAR(128) DEFAULT NULL,
+name CHAR(128) NOT NULL,
+city CHAR(128) NOT NULL,
 PRIMARY KEY (id)
 );
 
-
-CREATE TABLE account (
+CREATE TABLE account
+(
 id SMALLINT(5) AUTO_INCREMENT NOT NULL,
 username CHAR(128) NOT NULL,
 password CHAR(128) NOT NULL,
 isAdmin BOOLEAN DEFAULT 0,
 PRIMARY KEY (id)
 );
+
+CREATE TABLE flight
+(
+id SMALLINT(5) AUTO_INCREMENT NOT NULL,
+departing_from SMALLINT(5) NOT NULL,
+arriving_to SMALLINT(5) NOT NULL,
+time_departing DATETIME NOT NULL,
+price DOUBLE(10,2) NOT NULL,
+PRIMARY KEY (id)
+);
+
 INSERT INTO airport (name, city) VALUES ("Bromma","Stockholm"); 
 INSERT INTO airport (name, city) VALUES ("Arlanda","Stockholm");
-INSERT INTO airport (name, city) VALUES ("Landvetter","GÃ¶teborg");
-INSERT INTO airport (name, city) VALUES ("SmÃ¥land Airport","VÃ¤xjÃ¶");
-INSERT INTO airport (name, city) VALUES ("JÃ¶nkÃ¶ping Airport","JÃ¶nkÃ¶ping");
+INSERT INTO airport (name, city) VALUES ("Landvetter","Göteborg");
+INSERT INTO airport (name, city) VALUES ("Småland Airport","Växjö");
+INSERT INTO airport (name, city) VALUES ("Jönköping Airport","Jönköping");
 
 INSERT INTO account (username, password) VALUES ("Kristoffer","123");
 INSERT INTO account (username, password) VALUES ("Magnus","123");
