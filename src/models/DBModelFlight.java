b114bbabbf9b117f972f4c6226596a35c096ecae
Timalex <tim.alexander.gunnarsson@gmail.com>
2014-05-08 16:48:26 +0200

    DBModelFlight bör vara funktionell

--- src/models/DBModelFlight.java
+++ src/models/DBModelFlight.java
@@ -5,51 +5,74 @@ import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Vector;
 
-public class DBModelFlight {
+public class DBModelFlight
+{
 	private int id;
 	private DBModelAirport departingFrom;
 	private DBModelAirport arrivingTo;
 	private Date timeDeparting;
 	private double price;
 	
-	private String tableName = "flight";
-	private String columnDepartingFrom = "departing_from";
-	private String columnArrivingTo = "arriving_to";
-	private String columnTimeDeparting = "time_departing";
-	private String columnPrice = "price";
+	private static String TABLE_NAME = "flight";
+	private static String COLUMN_DEPARTING_FROM = "departing_from";
+	private static String COLUMN_ARRIVING_TO = "arriving_to";
+	private static String COLUMN_TIME_DEPARTING = "time_departing";
+	private static String COLUMN_PRICE = "price";
 	
-	public DBModelAirport getDepartingFrom() {
-		return departingFrom;
-	}
-	public DBModelAirport getArrivingTo() {
-		return arrivingTo;
-	}
-	public Date getTimeDeparting() {
-		return timeDeparting;
-	}
-	public double getPrice() {
-		return price;
-	}
-	public void setDepartingFrom(DBModelAirport departingFrom) {
+	public DBModelFlight(int id, DBModelAirport departingFrom, DBModelAirport arrivingTo, 
+				Date timeDeparting, double price) 
+	{
+		this.id = id;
 		this.departingFrom = departingFrom;
-	}
-	public void setArrivingTo(DBModelAirport arrivingTo) {
 		this.arrivingTo = arrivingTo;
-	}
-	public void setTimeDeparting(Date timeDeparting) {
 		this.timeDeparting = timeDeparting;
-	}
-	public void setPrice(double price) {
 		this.price = price;
+	}
+
+	public static Vector<DBModelFlight> getFlights()
+	{
+		Vector<DBModelFlight> flightList = new Vector<DBModelFlight>();
+
+			try (Connection conn = DBConnector.getConnection())
+			{
+				String query = "SELECT" + COLUMN_DEPARTING_FROM + "," + COLUMN_ARRIVING_TO + "," + COLUMN_TIME_DEPARTING + "," + COLUMN_PRICE + 
+						"FROM " + TABLE_NAME;
+				
+				Statement statement = conn.createStatement();
+				ResultSet result = statement.executeQuery(query);
+				
+				while (result.next())
+				{
+					int id = result.getInt(1);
+					int departingId = result.getInt(COLUMN_DEPARTING_FROM);
+					int arrivingId = result.getInt(COLUMN_ARRIVING_TO);
+					Date time = result.getDate(COLUMN_TIME_DEPARTING);
+					double price = result.getDouble(COLUMN_PRICE);
+					
+					DBModelAirport airportFrom = DBModelAirport.getAirport(departingId);
+					DBModelAirport airportTo = DBModelAirport.getAirport(arrivingId);
+					
+					flightList.add(new DBModelFlight(id, airportFrom, airportTo, time, price));
+				}
+				 
+			} 
+			catch (SQLException exception)
+			{
+				exception.printStackTrace();
+			}
+			
+		return flightList;
 	}
 	
 	public int insert()
 	{
 		try (Connection conn = DBConnector.getConnection())
 		{
-			String query = "INSERT INTO " + tableName + 
-					"(" + columnDepartingFrom + "," + columnArrivingTo + "," + columnTimeDeparting + "," + columnPrice + ") " + 
+			String query = "INSERT INTO " + TABLE_NAME + 
+					"(" + COLUMN_DEPARTING_FROM + "," + COLUMN_ARRIVING_TO + "," + COLUMN_TIME_DEPARTING + "," + COLUMN_PRICE + ") " + 
 					"VALUES (?, ?, ?, ?)";
 
 			PreparedStatement statement = conn.prepareStatement(query, PreparedStatement.RETURN_GENERATED_KEYS);
@@ -81,8 +104,8 @@ public class DBModelFlight {
 	{
 		try (Connection conn = DBConnector.getConnection())
 		{
-			String query = "UPDATE " + tableName + 
-						   " SET " + columnDepartingFrom + "=?, " + columnArrivingTo + "=? " + columnTimeDeparting + "=?, " + columnPrice + "=? " +
+			String query = "UPDATE " + TABLE_NAME + 
+						   " SET " + COLUMN_DEPARTING_FROM + "=?, " + COLUMN_ARRIVING_TO + "=? " + COLUMN_TIME_DEPARTING + "=?, " + COLUMN_PRICE + "=? " +
 						   "WHERE id=?";
 
 			PreparedStatement statement = conn.prepareStatement(query);
@@ -107,7 +130,7 @@ public class DBModelFlight {
 	{
 		try (Connection conn = DBConnector.getConnection())
 		{
-			String query = "DELETE FROM " + tableName +
+			String query = "DELETE FROM " + TABLE_NAME +
 						   " WHERE id=?";
 
 			PreparedStatement statement = conn.prepareStatement(query);
@@ -122,6 +145,31 @@ public class DBModelFlight {
 			exception.printStackTrace();
 		}
 		return -1;
+	}
+	
+	public DBModelAirport getDepartingFrom() {
+		return departingFrom;
+	}
+	public DBModelAirport getArrivingTo() {
+		return arrivingTo;
+	}
+	public Date getTimeDeparting() {
+		return timeDeparting;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setDepartingFrom(DBModelAirport departingFrom) {
+		this.departingFrom = departingFrom;
+	}
+	public void setArrivingTo(DBModelAirport arrivingTo) {
+		this.arrivingTo = arrivingTo;
+	}
+	public void setTimeDeparting(Date timeDeparting) {
+		this.timeDeparting = timeDeparting;
+	}
+	public void setPrice(double price) {
+		this.price = price;
 	}
 	
 }
