cdbdbf931b338b0365ff0cbf500023583431104f
Timalex <tim.alexander.gunnarsson@gmail.com>
2014-05-08 13:38:31 +0200

    Created insert(), update() and delete() in DBModelFlight

--- src/models/DBModelFlight.java
+++ src/models/DBModelFlight.java
@@ -1,6 +1,10 @@
 package models;
 
+import java.sql.Connection;
 import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class DBModelFlight {
 	private int id;
@@ -8,6 +12,12 @@ public class DBModelFlight {
 	private DBModelAirport arrivingTo;
 	private Date timeDeparting;
 	private double price;
+	
+	private String tableName = "flight";
+	private String columnDepartingFrom = "departing_from";
+	private String columnArrivingTo = "arriving_to";
+	private String columnTimeDeparting = "time_departing";
+	private String columnPrice = "price";
 	
 	public DBModelAirport getDepartingFrom() {
 		return departingFrom;
@@ -34,5 +44,84 @@ public class DBModelFlight {
 		this.price = price;
 	}
 	
+	public int insert()
+	{
+		try (Connection conn = DBConnector.getConnection())
+		{
+			String query = "INSERT INTO " + tableName + 
+					"(" + columnDepartingFrom + "," + columnArrivingTo + "," + columnTimeDeparting + "," + columnPrice + ") " + 
+					"VALUES (?, ?, ?, ?)";
+
+			PreparedStatement statement = conn.prepareStatement(query, PreparedStatement.RETURN_GENERATED_KEYS);
+
+			statement.setInt(1, departingFrom.getId());
+			statement.setInt(2, arrivingTo.getId());
+			statement.setDate(3, timeDeparting);
+			statement.setDouble(4, price);
+
+			int rowCount = statement.executeUpdate();
+
+
+			//Use the DB-generated id
+
+			ResultSet key = statement.getGeneratedKeys();
+			key.next();
+			id = key.getInt("GENERATED_KEY");
+
+			return rowCount;	 
+		} 
+		catch (SQLException exception) 
+		{
+			exception.printStackTrace();
+		}
+		return -1;
+	}
+
+	public int update()
+	{
+		try (Connection conn = DBConnector.getConnection())
+		{
+			String query = "UPDATE " + tableName + 
+						   " SET " + columnDepartingFrom + "=?, " + columnArrivingTo + "=? " + columnTimeDeparting + "=?, " + columnPrice + "=? " +
+						   "WHERE id=?";
+
+			PreparedStatement statement = conn.prepareStatement(query);
+			statement.setInt(1, departingFrom.getId());
+			statement.setInt(2, arrivingTo.getId());
+			statement.setDate(3, timeDeparting);
+			statement.setDouble(4, price);
+			statement.setInt(5,id);
+
+			int result = statement.executeUpdate();
+
+			return result;	 
+		} 
+		catch (SQLException exception) 
+		{
+			exception.printStackTrace();
+		}
+		return -1;
+	}
+
+	public int delete()
+	{
+		try (Connection conn = DBConnector.getConnection())
+		{
+			String query = "DELETE FROM " + tableName +
+						   " WHERE id=?";
+
+			PreparedStatement statement = conn.prepareStatement(query);
+			statement.setInt(1,id);
+
+			int result = statement.executeUpdate();
+
+			return result;	 
+		} 
+		catch (SQLException exception) 
+		{
+			exception.printStackTrace();
+		}
+		return -1;
+	}
 	
 }
