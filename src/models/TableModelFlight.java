20e81011a20c4fd14b4a598a4c1ec7c0c1eba60d
Timalex <tim.alexander.gunnarsson@gmail.com>
2014-05-16 14:05:34 +0200

    Nu finns konstanter(NAMN omg) på tidigare kolumner, de är också tillgängliga för andra klasser.

--- src/models/TableModelFlight.java
+++ src/models/TableModelFlight.java
@@ -10,7 +10,14 @@ public class TableModelFlight extends AbstractTableModel
   
 	private static final long serialVersionUID = 1L;
 	private Vector<DBModelFlight> data = new Vector<DBModelFlight>();
+	
+	public static final int ID_COLUMN = 0;
+	public static final int DEPARTING_FROM_COLUMN = 1;
+	public static final int ARRIVING_TO_COLUMN = 2;
+	public static final int DEPARTING_TIME_COLUMN = 3;
+	public static final int PRICE_COLUMN = 4;
 
+	
 	public void addFlight(DBModelFlight flight) {
 			data.add(flight);
 			fireTableDataChanged();
@@ -47,11 +54,6 @@ public class TableModelFlight extends AbstractTableModel
     	fireTableDataChanged();
     }
     
-    public TableModelFlight() {
-//    	TableCellEditor editor = new DefaultCellEditor(new JTextField());
-//    	setFlight(DBModelFlight.getFlights(1,1, new Date(2016,02,1)));
-	}
-    
     public DBModelFlight getFlight(int row)
     {
     	return data.get(row);
@@ -67,14 +69,16 @@ public class TableModelFlight extends AbstractTableModel
     public int getRowCount() {
         return data.size();
     }
+    
     @Override
-    public String getColumnName(int col) {
+    public String getColumnName(int col)
+    {
         switch (col) {
-        case 0: return "id";
-        case 1: return "Avreseort";
-        case 2: return "Destination";
-        case 3: return "Avgångstid";
-        case 4: return "Pris";
+        case ID_COLUMN: return "id";
+        case DEPARTING_FROM_COLUMN: return "Avreseort";
+        case ARRIVING_TO_COLUMN: return "Destination";
+        case DEPARTING_TIME_COLUMN: return "Avgångstid";
+        case PRICE_COLUMN: return "Pris";
         }
         return "";
     }
@@ -85,46 +89,22 @@ public class TableModelFlight extends AbstractTableModel
     	DBModelFlight rowData = (DBModelFlight) data.get(row);
         
         switch (col) {
-        case 0: return rowData.getId();
-        case 1: return rowData.getDepartingFrom();
-        case 2: return rowData.getArrivingTo();
-        case 3: return rowData.getTimeDeparting();
-        case 4: return rowData.getPrice();
-       
+        case ID_COLUMN: return rowData.getId();
+        case DEPARTING_FROM_COLUMN: return rowData.getDepartingFrom();
+        case ARRIVING_TO_COLUMN: return rowData.getArrivingTo();
+        case DEPARTING_TIME_COLUMN: return rowData.getTimeDeparting();
+        case PRICE_COLUMN: return rowData.getPrice();
         }        
         return "Unknown";
     }
-    
-   /*
-    *  @Override(non-Javadoc)
-    * @see javax.swing.table.AbstractTableModel#setValueAt(java.lang.Object, int, int)
-    */
-    
-    /*
-    public void setValueAt(Object cellData, int row, int col)
-    {
-    	DBModelFlight selectedflight = (DBModelFlight) data.get(row);
-    	
-        switch (col)
-        {
-	        case 1: selectedflight.setDepartingFrom((DBModelAirport) cellData);
-	        break;
-	        case 2: selectedflight.setArrivingTo((DBModelAirport) cellData);
-	        break;
-	        
-        }
-        selectedflight.update();
-    }
-   
-   */
     
     @Override
     public Class<?> getColumnClass(int col) {
         switch (col)
         {
-	        case 0: return Integer.class;
-	        case 1: return DBModelAirport.class;
-	        case 2: return DBModelAirport.class;
+	        case ID_COLUMN: return Integer.class;
+	        case DEPARTING_FROM_COLUMN: return DBModelAirport.class;
+	        case ARRIVING_TO_COLUMN: return DBModelAirport.class;
         }
         return Object.class;
     }
