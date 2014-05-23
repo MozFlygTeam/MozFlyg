eda6f71a13aa8fead7d9fe71764434b2bb3d9e37
Magnus Karlsson <magnus_karlsson@live.com>
2014-05-23 09:54:31 +0200

    Nu kan man få upp alla resor man bokat genom att trycka "Bokade resor" i edit flight-fönstret

--- src/GUI/WindowEditFlight.java
+++ src/GUI/WindowEditFlight.java
@@ -25,193 +25,192 @@ import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.ListSelectionListener;
 import javax.swing.table.TableColumn;
 
-	public class WindowEditFlight extends JFrame implements ActionListener, ListSelectionListener{
+  public class WindowEditFlight extends JFrame implements ActionListener, ListSelectionListener{
 
-		public DBModelFlight flightModel;
-		public static TableModelFlight tableModelFlight;
-		private JButton removeButton;
-		private JButton editButton;
-		private JButton bookedFlightsBtn;
-		private JButton bookFlightBtn;
-		private JButton unbookFlightsBtn;
-		private JTable flightTable;
-		
-		JComboBox<DBModelAirport> droppDownFrom;
-		JComboBox<DBModelAirport> droppDownTo;
-		JSpinner spinnerDate;
-		JButton searchButton;
-		
-		
-		private static final String SEARCH = "search";	
-		private static final String ADD = "add";
-		private static final String DELETE = "delete";
-		private static final String EDIT = "editera";
-		private static final String BOOKED = "booked";
-		private static final String BOOK = "book";
-		private static final String UNBOOK = "unbook";
-		
-		
-		
-		public WindowEditFlight() {
+    public DBModelFlight flightModel;
+    public static TableModelFlight tableModelFlight;
+    private JButton removeButton;
+    private JButton editButton;
+    private JButton bookedFlightsBtn;
+    private JButton bookFlightBtn;
+    private JButton unbookFlightsBtn;
+    private JTable flightTable;
+    
+    JComboBox<DBModelAirport> droppDownFrom;
+    JComboBox<DBModelAirport> droppDownTo;
+    JSpinner spinnerDate;
+    JButton searchButton;
+    
+    
+    private static final String SEARCH = "search";  
+    private static final String ADD = "add";
+    private static final String DELETE = "delete";
+    private static final String EDIT = "editera";
+    private static final String BOOKED = "booked";
+    private static final String BOOK = "book";
+    private static final String UNBOOK = "unbook";
+    
+    
+    
+    public WindowEditFlight() {
 
-			tableModelFlight = new TableModelFlight();
-			flightModel = new DBModelFlight();
-			flightTable = new JTable(tableModelFlight);
-			
-			
-			//��ndrar CellEditorn f��r avg��ngsflygets kolumn till en dropdownbox
-			TableColumn departingColumn = flightTable.getColumnModel().getColumn(TableModelFlight.DEPARTING_FROM_COLUMN);
-			departingColumn.setCellEditor(new DefaultCellEditor(new JComboBox<DBModelAirport>(DBModelAirport.getAll())));
-			
-			//��ndrar CellEditorn f��r ankomstflygets kolumn till en dropdownbox
-			TableColumn arrivingColumn = flightTable.getColumnModel().getColumn(TableModelFlight.ARRIVING_TO_COLUMN);
-			arrivingColumn.setCellEditor(new DefaultCellEditor(new JComboBox<DBModelAirport>(DBModelAirport.getAll())));
-			
-			flightTable.getSelectionModel().addListSelectionListener(this);
-		
-			JPanel searchPane = new JPanel();
-			 droppDownFrom = new JComboBox<DBModelAirport>(DBModelAirport.getAll());
-			 
-			 searchPane.add(droppDownFrom);
-			 
-			 droppDownTo = new JComboBox<DBModelAirport>(DBModelAirport.getAll()); 
-			 searchPane.add(droppDownTo);
-			 
-			 spinnerDate = new JSpinner(new SpinnerDateModel());
-			 searchPane.add(spinnerDate);
-			 
-			 searchButton = new JButton("Search");
-			 searchButton.addActionListener(this);
-			 searchButton.setActionCommand(SEARCH);
-			 searchPane.add(searchButton);
+      tableModelFlight = new TableModelFlight();
+      flightModel = new DBModelFlight();
+      flightTable = new JTable(tableModelFlight);
+      
+      
+      //��ndrar CellEditorn f��r avg��ngsflygets kolumn till en dropdownbox
+      TableColumn departingColumn = flightTable.getColumnModel().getColumn(TableModelFlight.DEPARTING_FROM_COLUMN);
+      departingColumn.setCellEditor(new DefaultCellEditor(new JComboBox<DBModelAirport>(DBModelAirport.getAll())));
+      
+      //��ndrar CellEditorn f��r ankomstflygets kolumn till en dropdownbox
+      TableColumn arrivingColumn = flightTable.getColumnModel().getColumn(TableModelFlight.ARRIVING_TO_COLUMN);
+      arrivingColumn.setCellEditor(new DefaultCellEditor(new JComboBox<DBModelAirport>(DBModelAirport.getAll())));
+      
+      flightTable.getSelectionModel().addListSelectionListener(this);
+    
+      JPanel searchPane = new JPanel();
+       droppDownFrom = new JComboBox<DBModelAirport>(DBModelAirport.getAll());
+       
+       searchPane.add(droppDownFrom);
+       
+       droppDownTo = new JComboBox<DBModelAirport>(DBModelAirport.getAll()); 
+       searchPane.add(droppDownTo);
+       
+       spinnerDate = new JSpinner(new SpinnerDateModel());
+       searchPane.add(spinnerDate);
+       
+       searchButton = new JButton("Search");
+       searchButton.addActionListener(this);
+       searchButton.setActionCommand(SEARCH);
+       searchPane.add(searchButton);
 
-			JPanel contentPane = new JPanel(new BorderLayout());
-			JScrollPane scrollPane = new JScrollPane(flightTable);
-			JPanel bottomPanel = new JPanel(new FlowLayout());
+      JPanel contentPane = new JPanel(new BorderLayout());
+      JScrollPane scrollPane = new JScrollPane(flightTable);
+      JPanel bottomPanel = new JPanel(new FlowLayout());
 
-			setContentPane(contentPane);
-			add(searchPane, BorderLayout.NORTH);
-			add(scrollPane, BorderLayout.CENTER);
-			add(bottomPanel, BorderLayout.PAGE_END);
-			
-			
-			if(DBModelAccount.loggedInUser.isAdmin()){
-				
-			JButton addButton = new JButton("L��gg till");
-			addButton.addActionListener(this);
-			addButton.setActionCommand(ADD);
+      setContentPane(contentPane);
+      add(searchPane, BorderLayout.NORTH);
+      add(scrollPane, BorderLayout.CENTER);
+      add(bottomPanel, BorderLayout.PAGE_END);
+      
+      
+      if(DBModelAccount.loggedInUser.isAdmin()){
+        
+      JButton addButton = new JButton("L��gg till");
+      addButton.addActionListener(this);
+      addButton.setActionCommand(ADD);
 
-			removeButton = new JButton("Ta Bort");
-			removeButton.addActionListener(this);
-			removeButton.setActionCommand(DELETE);
-			removeButton.setEnabled(false);
-			
-			
-			editButton = new JButton("Editera");
-			editButton.addActionListener(this);
-			editButton.setActionCommand(EDIT);
-			editButton.setEnabled(false);
-			
-			bottomPanel.add(addButton);
-			bottomPanel.add(removeButton);
-			bottomPanel.add(editButton);
-			
-			
-			}
-			else{
-				
-				bookedFlightsBtn = new JButton("Bokade resor");
-				bookedFlightsBtn.addActionListener(this);
-				bookedFlightsBtn.setActionCommand(BOOKED);
-				bookedFlightsBtn.setEnabled(true);
-				
-				
-				bookFlightBtn = new JButton("Booka flyg");
-				bookFlightBtn.addActionListener(this);
-				bookFlightBtn.setActionCommand(BOOK);
-				bookFlightBtn.setEnabled(true);
-				
-				unbookFlightsBtn = new JButton("Avboka flyg");
-				unbookFlightsBtn.addActionListener(this);
-				unbookFlightsBtn.setActionCommand(UNBOOK);
-				unbookFlightsBtn.setEnabled(true);
-				
-				
-				
-				bottomPanel.add(bookedFlightsBtn);
-				bottomPanel.add(bookFlightBtn);
-				bottomPanel.add(unbookFlightsBtn);
-				
-				
-			}
-	
-			
-			
-			pack();
-			setLocationRelativeTo(null);
-		} 
-		
-	
-		private void addFlight() {
-			
-			WindowAddFlight add = new WindowAddFlight();
-			add.setVisible(true);
-			
-			DBModelFlight model =  add.getFlight();
-			if(model != null){
-			
-				if (model.insert() == 1) {
-				tableModelFlight.addFlight(model);
-				}
-			}
-			
-		}
-		
-		@Override
-		public void actionPerformed(ActionEvent event) {
+      removeButton = new JButton("Ta Bort");
+      removeButton.addActionListener(this);
+      removeButton.setActionCommand(DELETE);
+      removeButton.setEnabled(false);
+      
+      
+      editButton = new JButton("Editera");
+      editButton.addActionListener(this);
+      editButton.setActionCommand(EDIT);
+      editButton.setEnabled(false);
+      
+      bottomPanel.add(addButton);
+      bottomPanel.add(removeButton);
+      bottomPanel.add(editButton);
+      
+      
+      }
+      else{
+        
+        bookedFlightsBtn = new JButton("Bokade resor");
+        bookedFlightsBtn.addActionListener(this);
+        bookedFlightsBtn.setActionCommand(BOOKED);
+        bookedFlightsBtn.setEnabled(true);
+        
+        
+        bookFlightBtn = new JButton("Booka flyg");
+        bookFlightBtn.addActionListener(this);
+        bookFlightBtn.setActionCommand(BOOK);
+        bookFlightBtn.setEnabled(true);
+        
+        unbookFlightsBtn = new JButton("Avboka flyg");
+        unbookFlightsBtn.addActionListener(this);
+        unbookFlightsBtn.setActionCommand(UNBOOK);
+        unbookFlightsBtn.setEnabled(true);
+        
+        
+        
+        bottomPanel.add(bookedFlightsBtn);
+        bottomPanel.add(bookFlightBtn);
+        bottomPanel.add(unbookFlightsBtn);
+        
+        
+      }
+  
+      
+      
+      pack();
+      setLocationRelativeTo(null);
+    } 
+    
+  
+    private void addFlight() {
+      
+      WindowAddFlight add = new WindowAddFlight();
+      add.setVisible(true);
+      
+      DBModelFlight model =  add.getFlight();
+      if(model != null){
+      
+        if (model.insert() == 1) {
+        tableModelFlight.addFlight(model);
+        }
+      }
+      
+    }
+    
+    @Override
+    public void actionPerformed(ActionEvent event) {
 
-			switch (event.getActionCommand()) {
-			case ADD:
-				addFlight();
-				break;
-			case DELETE:
-				int i = flightTable.getSelectedRow();
-				tableModelFlight.removeFlight(i);
-				break;
-			case SEARCH:
-				DBModelAirport arFr = (DBModelAirport) droppDownFrom.getSelectedItem();
-				DBModelAirport arTo = (DBModelAirport) droppDownTo.getSelectedItem();
-				SpinnerDateModel sp = (SpinnerDateModel)spinnerDate.getModel();
-				
-				java.sql.Date sqlDate = new java.sql.Date(sp.getDate().getTime());
-				System.out.print("SEARCH METODEN !!");
-				tableModelFlight.setFlight(DBModelFlight.getFlights(arFr.getId(),arTo.getId(), sqlDate));
-				
-				break;
-			case BOOKED:
-				
-				
-				// tableModelFlight.setFlight(DBModelBookedFlight.getAllBookedFlights());
-				
-				break;
-			case BOOK:
-				break;
-			case UNBOOK:
-				break;
-			
-			}
-		
-			
-			}
-		
-		public void valueChanged(ListSelectionEvent event)
-		{
-			if(DBModelAccount.loggedInUser.isAdmin()){
-				removeButton.setEnabled(flightTable.getSelectedRowCount() > 0);
-				editButton.setEnabled(flightTable.getSelectedRowCount() > 0);
-			}
-		}
-	}
-	
-	
+      switch (event.getActionCommand()) {
+      case ADD:
+        addFlight();
+        break;
+      case DELETE:
+        int i = flightTable.getSelectedRow();
+        tableModelFlight.removeFlight(i);
+        break;
+      case SEARCH:
+        DBModelAirport arFr = (DBModelAirport) droppDownFrom.getSelectedItem();
+        DBModelAirport arTo = (DBModelAirport) droppDownTo.getSelectedItem();
+        SpinnerDateModel sp = (SpinnerDateModel)spinnerDate.getModel();
+        
+        java.sql.Date sqlDate = new java.sql.Date(sp.getDate().getTime());
+        System.out.print("SEARCH METODEN !!");
+        tableModelFlight.setFlight(DBModelFlight.getFlights(arFr.getId(),arTo.getId(), sqlDate));
+        
+        break;
+      case BOOKED:
+        
+        tableModelFlight.setFlight(DBModelFlight.getBookedFlights());
+        
+        break;
+      case BOOK:
+        break;
+      case UNBOOK:
+        break;
+      
+      }
+    
+      
+      }
+    
+    public void valueChanged(ListSelectionEvent event)
+    {
+      if(DBModelAccount.loggedInUser.isAdmin()){
+        removeButton.setEnabled(flightTable.getSelectedRowCount() > 0);
+        editButton.setEnabled(flightTable.getSelectedRowCount() > 0);
+      }
+    }
+  }
+  
+  
 
--- src/models/DBModelFlight.java
+++ src/models/DBModelFlight.java
@@ -9,193 +9,241 @@ import java.util.Vector;
 
 public class DBModelFlight
 {
-	private int id;
-	private DBModelAirport departingFrom;
-	private DBModelAirport arrivingTo;
-	private Date timeDeparting;
-	private double price;
+  private int id;
+  private DBModelAirport departingFrom;
+  private DBModelAirport arrivingTo;
+  private Date timeDeparting;
+  private double price;
 
-	
-	private static String TABLE_NAME = "flight";
-	private static String COLUMN_DEPARTING_FROM = "departing_from";
-	private static String COLUMN_ARRIVING_TO = "arriving_to";
-	private static String COLUMN_TIME_DEPARTING = "time_departing";
-	private static String COLUMN_PRICE = "price";
-	
-	public DBModelFlight(int id, DBModelAirport departingFrom, DBModelAirport arrivingTo, 
-				Date timeDeparting,double price) 
-	{
-		this.id = id;
-		this.departingFrom = departingFrom;
-		this.arrivingTo = arrivingTo;
-		this.timeDeparting = timeDeparting;
-		this.price = price;
-	}
-	
-	public DBModelFlight(DBModelAirport departingFrom, DBModelAirport arrivingTo, 
-			Date timeDeparting,double price) 
-		{
-		this.departingFrom = departingFrom;
-		this.arrivingTo = arrivingTo;
-		this.timeDeparting = timeDeparting;
-		this.price = price;
-		}
+  
+  private static final String TABLE_FLIGHT = "flight";
+  private static final String COLUMN_DEPARTING_FROM = "departing_from";
+  private static final String COLUMN_ARRIVING_TO = "arriving_to";
+  private static final String COLUMN_TIME_DEPARTING = "time_departing";
+  private static final String COLUMN_PRICE = "price";
+  private static final String COLUMN_ACCOUNT_ID = "account_id";
+  private static final String COLUMN_FLIGHT_ID = "flight_id";
+  private static final String TABLE_BOOKED_FLIGHT = "booked_flight";
+  private static final String TABLE_ACCOUNT = "account";
+  
+  public DBModelFlight(int id, DBModelAirport departingFrom, DBModelAirport arrivingTo, 
+        Date timeDeparting,double price) 
+  {
+    this.id = id;
+    this.departingFrom = departingFrom;
+    this.arrivingTo = arrivingTo;
+    this.timeDeparting = timeDeparting;
+    this.price = price;
+  }
+  
+  public DBModelFlight(DBModelAirport departingFrom, DBModelAirport arrivingTo, 
+      Date timeDeparting,double price) 
+    {
+    this.departingFrom = departingFrom;
+    this.arrivingTo = arrivingTo;
+    this.timeDeparting = timeDeparting;
+    this.price = price;
+    }
 
-	public DBModelFlight() {}
+  public DBModelFlight() {}
 
-	public static Vector<DBModelFlight> getFlights(int fromAirportId, int toAirportId, Date date)
-	{
-		Vector<DBModelFlight> flightList = new Vector<DBModelFlight>();
+  public static Vector<DBModelFlight> getFlights(int fromAirportId, int toAirportId, Date date)
+  {
+    Vector<DBModelFlight> flightList = new Vector<DBModelFlight>();
 
-			try (Connection conn = DBConnector.getConnection())
-			{
-				String query = "SELECT" + " id, " + COLUMN_DEPARTING_FROM + ", " + COLUMN_ARRIVING_TO + ", " + COLUMN_TIME_DEPARTING + ", " + COLUMN_PRICE + 
-						" FROM " + TABLE_NAME +
-						" WHERE " + COLUMN_DEPARTING_FROM + "=? AND " + COLUMN_ARRIVING_TO + "=? AND " + "DATE("+COLUMN_TIME_DEPARTING+")" + " =?";
-				
-				PreparedStatement statement = conn.prepareStatement(query);
-				statement.setInt(1, fromAirportId);
-				statement.setInt(2, toAirportId);
-				statement.setString(3, date.toString());
-				
-				ResultSet result = statement.executeQuery();
-				
-				while (result.next())
-				{
-					int id = result.getInt(1);
-					int departingId = result.getInt(COLUMN_DEPARTING_FROM);
-					int arrivingId = result.getInt(COLUMN_ARRIVING_TO);
-					Date departingTime = result.getDate(COLUMN_TIME_DEPARTING);
-					double price = result.getDouble(COLUMN_PRICE);
-					
-					DBModelAirport airportFrom = DBModelAirport.getAirport(departingId);
-					DBModelAirport airportTo = DBModelAirport.getAirport(arrivingId);
-					
-					flightList.add(new DBModelFlight(id, airportFrom, airportTo, departingTime, price));
-				}
-				 
-			} 
-			catch (SQLException exception)
-			{
-				exception.printStackTrace();
-			}
-			
-		return flightList;
-	}
-	
-	public int insert()
-	{
-		try (Connection conn = DBConnector.getConnection())
-		{
-			String query = "INSERT INTO " + TABLE_NAME + 
-					"(" + COLUMN_DEPARTING_FROM + "," + COLUMN_ARRIVING_TO + "," + COLUMN_TIME_DEPARTING + "," + COLUMN_PRICE + ") " + 
-					"VALUES (?, ?, ?, ?)";
+      try (Connection conn = DBConnector.getConnection())
+      {
+        String query = "SELECT" + " id, " + COLUMN_DEPARTING_FROM + ", " + COLUMN_ARRIVING_TO + ", " + COLUMN_TIME_DEPARTING + ", " + COLUMN_PRICE + 
+            " FROM " + TABLE_FLIGHT +
+            " WHERE " + COLUMN_DEPARTING_FROM + "=? AND " + COLUMN_ARRIVING_TO + "=? AND " + "DATE("+COLUMN_TIME_DEPARTING+")" + " =?";
+        
+        PreparedStatement statement = conn.prepareStatement(query);
+        statement.setInt(1, fromAirportId);
+        statement.setInt(2, toAirportId);
+        statement.setString(3, date.toString() );
+        
+        System.out.println(statement.toString());
+        
+        ResultSet result = statement.executeQuery();
+        
+        while (result.next())
+        {
+          int id = result.getInt(1);
+          int departingId = result.getInt(COLUMN_DEPARTING_FROM);
+          int arrivingId = result.getInt(COLUMN_ARRIVING_TO);
+          Date departingTime = result.getDate(COLUMN_TIME_DEPARTING);
+          double price = result.getDouble(COLUMN_PRICE);
+          
+          DBModelAirport airportFrom = DBModelAirport.getAirport(departingId);
+          DBModelAirport airportTo = DBModelAirport.getAirport(arrivingId);
+          
+          flightList.add(new DBModelFlight(id, airportFrom, airportTo, departingTime, price));
+        }
+         
+      } 
+      catch (SQLException exception)
+      {
+        exception.printStackTrace();
+      }
+      
+    return flightList;
+  }
+  
+  public static Vector<DBModelFlight> getBookedFlights()
+  {
+    Vector<DBModelFlight> flightList = new Vector<DBModelFlight>();
+    
+      try (Connection conn = DBConnector.getConnection())
+      {
+        String query = "SELECT " + COLUMN_DEPARTING_FROM + ", " + COLUMN_ARRIVING_TO + ", " + COLUMN_TIME_DEPARTING + ", " + COLUMN_PRICE + 
+                 " FROM " + TABLE_ACCOUNT +
+                 " INNER JOIN " + TABLE_BOOKED_FLIGHT + " ON " + COLUMN_ACCOUNT_ID + " = account.id " + 
+                 " INNER JOIN " + TABLE_FLIGHT + " ON " + COLUMN_FLIGHT_ID + " = flight.id" + 
+                 " WHERE " + COLUMN_ACCOUNT_ID + "=?";
+        
+        PreparedStatement statement = conn.prepareStatement(query);
+        statement.setInt(1, DBModelAccount.loggedInUser.getId() );
+        
+        System.out.println(statement.toString());
+        
+        ResultSet result = statement.executeQuery();
+        
+        while (result.next())
+        {
+          int id = result.getInt(1);
+          int departingId = result.getInt(COLUMN_DEPARTING_FROM);
+          int arrivingId = result.getInt(COLUMN_ARRIVING_TO);
+          Date departingTime = result.getDate(COLUMN_TIME_DEPARTING);
+          double price = result.getDouble(COLUMN_PRICE);
+          
+          DBModelAirport airportFrom = DBModelAirport.getAirport(departingId);
+          DBModelAirport airportTo = DBModelAirport.getAirport(arrivingId);
+          
+          flightList.add(new DBModelFlight(id, airportFrom, airportTo, departingTime, price));
+        }
+         
+      } 
+      catch (SQLException exception)
+      {
+        exception.printStackTrace();
+      }
+      
+    return flightList;
+  }
+  
+  public int insert()
+  {
+    try (Connection conn = DBConnector.getConnection())
+    {
+      String query = "INSERT INTO " + TABLE_FLIGHT + 
+          "(" + COLUMN_DEPARTING_FROM + "," + COLUMN_ARRIVING_TO + "," + COLUMN_TIME_DEPARTING + "," + COLUMN_PRICE + ") " + 
+          "VALUES (?, ?, ?, ?)";
 
-			PreparedStatement statement = conn.prepareStatement(query, PreparedStatement.RETURN_GENERATED_KEYS);
+      PreparedStatement statement = conn.prepareStatement(query, PreparedStatement.RETURN_GENERATED_KEYS);
 
-			statement.setInt(1, departingFrom.getId());
-			statement.setInt(2, arrivingTo.getId());
-			statement.setDate(3, timeDeparting);
-			statement.setDouble(5, price);
+      statement.setInt(1, departingFrom.getId());
+      statement.setInt(2, arrivingTo.getId());
+      statement.setDate(3, timeDeparting);
+      statement.setDouble(5, price);
 
-			int rowCount = statement.executeUpdate();
+      int rowCount = statement.executeUpdate();
 
 
-			//Use the DB-generated id
+      //Use the DB-generated id
 
-			ResultSet key = statement.getGeneratedKeys();
-			key.next();
-			id = key.getInt("GENERATED_KEY");
+      ResultSet key = statement.getGeneratedKeys();
+      key.next();
+      id = key.getInt("GENERATED_KEY");
 
-			return rowCount;	 
-		} 
-		catch (SQLException exception) 
-		{
-			exception.printStackTrace();
-		}
-		return -1;
-	}
+      return rowCount;   
+    } 
+    catch (SQLException exception) 
+    {
+      exception.printStackTrace();
+    }
+    return -1;
+  }
 
-	public int update()
-	{
-		try (Connection conn = DBConnector.getConnection())
-		{
-			String query = "UPDATE " + TABLE_NAME + 
-						   " SET " + COLUMN_DEPARTING_FROM + "=?, " + COLUMN_ARRIVING_TO + "=?, " + COLUMN_TIME_DEPARTING + "=?, " + COLUMN_PRICE + "=? " +
-						   "WHERE id=?";
+  public int update()
+  {
+    try (Connection conn = DBConnector.getConnection())
+    {
+      String query = "UPDATE " + TABLE_FLIGHT + 
+               " SET " + COLUMN_DEPARTING_FROM + "=?, " + COLUMN_ARRIVING_TO + "=?, " + COLUMN_TIME_DEPARTING + "=?, " + COLUMN_PRICE + "=? " +
+               "WHERE id=?";
 
-			PreparedStatement statement = conn.prepareStatement(query);
-			statement.setInt(1, departingFrom.getId());
-			statement.setInt(2, arrivingTo.getId());
-			statement.setDate(3, timeDeparting);
-			statement.setDouble(4, price);
-			statement.setInt(5,id);
-			
-			
-			int result = statement.executeUpdate();
+      PreparedStatement statement = conn.prepareStatement(query);
+      statement.setInt(1, departingFrom.getId());
+      statement.setInt(2, arrivingTo.getId());
+      statement.setDate(3, timeDeparting);
+      statement.setDouble(4, price);
+      statement.setInt(5,id);
+      
+      
+      int result = statement.executeUpdate();
 
-			return result;	 
-		} 
-		catch (SQLException exception) 
-		{
-			exception.printStackTrace();
-		}
-		return -1;
-	}
+      return result;   
+    } 
+    catch (SQLException exception) 
+    {
+      exception.printStackTrace();
+    }
+    return -1;
+  }
 
-	public int delete()
-	{
-		try (Connection conn = DBConnector.getConnection())
-		{
-			String query = "DELETE FROM " + TABLE_NAME +
-						   " WHERE id=?";
+  public int delete()
+  {
+    try (Connection conn = DBConnector.getConnection())
+    {
+      String query = "DELETE FROM " + TABLE_FLIGHT +
+               " WHERE id=?";
 
-			PreparedStatement statement = conn.prepareStatement(query);
-			statement.setInt(1,id);
+      PreparedStatement statement = conn.prepareStatement(query);
+      statement.setInt(1,id);
 
-			int result = statement.executeUpdate();
+      int result = statement.executeUpdate();
 
-			return result;	 
-		} 
-		catch (SQLException exception) 
-		{
-			exception.printStackTrace();
-		}
-		return -1;
-	}
-	
-	public DBModelAirport getDepartingFrom() {
-		return departingFrom;
-	}
-	public DBModelAirport getArrivingTo() {
-		return arrivingTo;
-	}
-	public Date getTimeDeparting() {
-		return timeDeparting;
-	}
-	
-	public double getPrice() {
-		return price;
-	}
-	public void setDepartingFrom(DBModelAirport departingFrom) {
-		this.departingFrom = departingFrom;
-	}
-	public void setArrivingTo(DBModelAirport arrivingTo) {
-		this.arrivingTo = arrivingTo;
-	}
-	public void setTimeDeparting(Date timeDeparting) {
-		this.timeDeparting = timeDeparting;
-	}
-	
-	public void setPrice(double price) {
-		this.price = price;
-	}
+      return result;   
+    } 
+    catch (SQLException exception) 
+    {
+      exception.printStackTrace();
+    }
+    return -1;
+  }
+  
+  public DBModelAirport getDepartingFrom() {
+    return departingFrom;
+  }
+  public DBModelAirport getArrivingTo() {
+    return arrivingTo;
+  }
+  public Date getTimeDeparting() {
+    return timeDeparting;
+  }
+  
+  public double getPrice() {
+    return price;
+  }
+  public void setDepartingFrom(DBModelAirport departingFrom) {
+    this.departingFrom = departingFrom;
+  }
+  public void setArrivingTo(DBModelAirport arrivingTo) {
+    this.arrivingTo = arrivingTo;
+  }
+  public void setTimeDeparting(Date timeDeparting) {
+    this.timeDeparting = timeDeparting;
+  }
+  
+  public void setPrice(double price) {
+    this.price = price;
+  }
 
-	public int getId() {
-		return id;
-	}
+  public int getId() {
+    return id;
+  }
 
-	
+  
 
-	
+  
 }
